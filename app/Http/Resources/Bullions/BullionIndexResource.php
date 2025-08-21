1682e9e99abde61a52e96780b0ca688eefba0c9d
Abdelrahman <abdelrahman.mohsen.elsaied94@gmail.com>
2025-08-21 19:28:02 +0300

    update in bullion percentage 8

--- app/Http/Resources/Bullions/BullionIndexResource.php
+++ app/Http/Resources/Bullions/BullionIndexResource.php
@@ -24,11 +24,11 @@ class BullionIndexResource extends JsonResource
             // 'dollar_price' => number_format($this->dollar_price, 2),
 
             'base_price' => number_format(
-                $this->base_price + ($this->base_price * ($this->bullion->percentage_increase ?? 0) / 100),
+                (float)$this->base_price + ((float)$this->base_price * ((float)($this->bullion->percentage_increase ?? 0) / 100)),
                 2
             ),
             'dollar_price' => number_format(
-                $this->dollar_price + ($this->dollar_price * ($this->bullion->percentage_increase ?? 0) / 100),
+                (float)$this->dollar_price + ((float)$this->dollar_price * ((float)($this->bullion->percentage_increase ?? 0) / 100)),
                 2
             ),
             'latest_updated' => (string) Carbon::parse($this->updated_at)->valueOf(),
