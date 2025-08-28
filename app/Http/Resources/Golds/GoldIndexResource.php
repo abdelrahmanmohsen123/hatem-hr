46242f3235b4dfa2d9f0e8ac94dbd08adf0be018
Abdelrahman <abdelrahman.mohsen.elsaied94@gmail.com>
2025-08-28 16:52:18 +0300

    update in bullion percentage 2

--- app/Http/Resources/Golds/GoldIndexResource.php
+++ app/Http/Resources/Golds/GoldIndexResource.php
@@ -16,18 +16,22 @@ class GoldIndexResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        // Ensure numeric values are properly cast to float before formatting
+        $basePrice = is_numeric($this->base_price) ? (float) $this->base_price : 0.0;
+        $dollarPrice = is_numeric($this->dollar_price) ? (float) $this->dollar_price : 0.0;
+        $changeAmount = is_numeric($this->change_amount) ? (float) $this->change_amount : 0.0;
         return [
             'id' => $this->id,
             'name' =>$this->gold->{'name_'.$request->header('lang')},
             'icon' =>uploadsPath($this->gold->icon),
-            'base_price' => number_format($this->base_price,2),
-            'dollar_price' => number_format($this->dollar_price,2),
+            'base_price' => number_format($basePrice,2),
+            'dollar_price' => number_format($dollarPrice,2),
             'base_price_nonformate'=> (float) round($this->base_price,5),
             'dollar_price_nonformate'=> (float) round($this->dollar_price,5),
 
 
             'status_price' => $this->status_price,
-             'change_amount' => $this->change_amount ? number_format($this->change_amount,1) : '0',
+             'change_amount' => $changeAmount ? number_format($changeAmount,1) : '0',
             'latest_updated' => (string) Carbon::parse($this->latest_updated)->valueOf(),
         ];
     }
